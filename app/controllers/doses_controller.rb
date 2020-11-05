9270ee5615e3ba799be4c030f18c349e2dd89c0d
Alexandre Vieira <alexandre.ealimentos@gmail.com>
2020-11-05 20:15:42 +0900

    Created cocktail and dose creation methods. Added Delete dose method

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -3,18 +3,31 @@ class DosesController < ApplicationController
   def create
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail = @dose
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktails_path
     else
       render :new
+    end
   end  
-
 
   def new
     @dose = Dose.new
     @cocktail = Cocktail.find(params[:cocktail_id])
 
   end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+  
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end 
+
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails do
-    resources :doses, only: [ :show, :new, :create ] 
+    resources :doses, only: [ :show, :new, :create, :destroy ] 
     # !! should I use only, if I am not using scaffold?
   end
-  
+  resources :doses, only: [ :show, :destroy ]
 end
 
